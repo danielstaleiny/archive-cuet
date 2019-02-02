88599ea67484b673599e082962e07295ee4482df
danielstaleiny <daniel.rafaj@gmail.com>
2019-02-02 15:06:33 +0100

    docker-compose pg setup, test request

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-require("dotenv").config(); // load .env
+require('dotenv').config() // load .env
+const cheerio = require('cheerio')
+const rp = require('request-promise-native')
 
-const { log } = console;
-const { URL } = process.env;
+const https = `https://`
 
-log("init");
+const { URL } = process.env
+if (!URL) throw new Error("URL is not define, don't know what to scrape.")
 
-log("URL env test: ", URL);
+const log = (...args) =>
+    console.log(`${new Date().toISOString()} ${args.join('')}`)
+
+const err = (...args) =>
+    console.trace(`${new Date().toISOString()} ${args.join('')}`)
+
+log('Scraping domain: ', URL)
+
+const run = async () => {
+    const res = await rp(`${https}${URL}`)
+    log(res)
+}
+
+run().catch(err)
