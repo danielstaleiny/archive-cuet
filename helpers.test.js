7ac4a9f306c6841a71537d6f50aa51ac353c4af2
danielstaleiny <daniel.rafaj@tuta.io>
2019-02-03 23:19:52 +0100

    test addQuery

--- helpers.test.js
+++ helpers.test.js
@@ -1,15 +1,20 @@
 const { log, addQuery, parseAttachmentInfo, sizeParse } = require('./helpers')
 
 describe('helpers.js', () => {
-    describe('log', () => {
-        test('test', () => {
-            expect('a').toBe('a')
-        })
-    })
-
     describe('addQuery', () => {
-        test('test', () => {
-            expect('a').toBe('a')
+        test('test object in, string out', () => {
+            expect(addQuery({ first: '1', second: '2', third: '3' })).toBe(
+                '?first=1&second=2&third=3'
+            )
+        })
+        test('test 1 value', () => {
+            expect(addQuery({ first: '1' })).toBe('?first=1')
+        })
+        test('test empty object', () => {
+            expect(addQuery({})).toBe('')
+        })
+        test('test no value', () => {
+            expect(addQuery()).toBe('')
         })
     })
 
